9ad1bb71b32cc02544fb27766d0f2699f6fd78c4
Sebastian Kisiolek <sebastian@softwarepark.pl>
2023-09-07 14:22:35 +0200

    initial commit

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import ProductList from "@/ui/organisms/ProductList";
+import { ProductItemType } from "@/ui/types";
+
+const products: ProductItemType[] = [
+  {
+    id: "1",
+    name: "Plecak",
+    price: 2999,
+    category: "Akcesoria",
+    coverImage: {
+      src: "/backpack.jpg",
+      alt: "Plecak",
+    },
+  },
+  {
+    id: "2",
+    name: "Kubek",
+    price: 1099,
+    category: "Akcesoria",
+    coverImage: {
+      src: "/mug.jpg",
+      alt: "Kubek",
+    },
+  },
+  {
+    id: "3",
+    name: "Czapka",
+    price: 1099,
+    category: "Akcesoria",
+    coverImage: {
+      src: "/snapback.jpg",
+      alt: "Czapka",
+    },
+  },
+  {
+    id: "3",
+    name: "Bluza",
+    price: 10099,
+    category: "Akcesoria",
+    coverImage: {
+      src: "/unisex_long_sleeve_tee.jpg",
+      alt: "Czapka",
+    },
+  },
+];
 
 const Products = () => {
-  return <div>page</div>;
+  return (
+    <section className="mx-auto flex justify-center px-5 my-7">
+      <ProductList products={products} />;
+    </section>
+  );
 };
 
 export default Products;
