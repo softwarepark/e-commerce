f3e158785fe9dcf7cdcac1f2c05ad2a3bdef4ef3
Sebastian Kisiolek <sebastian@softwarepark.pl>
2023-09-08 11:36:03 +0200

    Node version change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
-    <section className="mx-auto flex justify-center px-5 my-7">
+    <section className="mx-auto flex flex-col items-center px-5 my-7">
       Homepage
+      <Link href="/products">
+        <button className="px-2 py-1 bg-slate-300 rounded">
+          Go to product
+        </button>
+      </Link>
     </section>
   );
 }
