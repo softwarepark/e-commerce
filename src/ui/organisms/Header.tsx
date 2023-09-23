3d94363331b4df62b2ea5302b9a101defb94eddf
Sebastian Kisiolek <sebastian@softwarepark.pl>
2023-09-23 21:24:22 +0200

    change menu

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -3,28 +3,47 @@ import ActiveLink from "../atoms/ActiveLink";
 import { Route } from "next";
 import { Card } from "@/ui/molecules/Card";
 
+const menuItems = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "All",
+    path: "/products/1",
+  },
+  {
+    label: "Categories",
+    path: "/categories",
+  },
+  {
+    label: "About",
+    path: "/about",
+  },
+  {
+    label: "Contact",
+    path: "/contact",
+  },
+  {
+    label: "Admin",
+    path: "/admin",
+  },
+];
+
 export const Header = () => {
   return (
     <header className="sticky top-0 bg-white border-b bg-opacity-60 backdrop-blur-lg z-50">
       <div className="mx-auto container px-2 sm:px-4 lg:px-8">
         <div className="flex justify-between gap-x-4 flex-row items-center pb-0">
-          <nav className="scrolling-touch scroll-shadows flex overflow-x-scrolllg:h-16 lg:overflow-x-auto">
+          <nav className="scrolling-touch scroll-shadows flex overflow-x-scrolllg:h-16 lg:overflow-x-auto scrollbar-none max-w-[15rem] sm:max-w-none overflow-x-auto">
             <ul className="flex h-16 max-w-full space-x-4 whitespace-nowrap">
-              <li>
-                <ActiveLink exact={false} href="/">
-                  Home
-                </ActiveLink>
-              </li>
-              <li>
-                <ActiveLink exact={false} href={"/products/1" as Route}>
-                  All
-                </ActiveLink>
-              </li>
-              <li>
-                <ActiveLink exact={false} href={"/categories" as Route}>
-                  Categories
-                </ActiveLink>
-              </li>
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <ActiveLink exact={false} href={item.path as Route}>
+                    {item.label}
+                  </ActiveLink>
+                </li>
+              ))}
             </ul>
           </nav>
           <Card />
